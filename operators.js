92a12cf72a137aa47f7391e6123c28863b33117e
Vinoth kumar A <vinothkumarfrta@gmail.com>
2014-10-22 06:55:13 +0000

    Complete Operators

---
+++ operators.js
@@ -0,0 +1,35 @@
+//Operators- Utter Basic
+
+//Number
+console.log(2.664e9) //Exponent
+console.log(21 / 0) //Infinity
+console.log(0 / 0) //NaN
+
+//String
+console.log("Hello with \n line") //escape chars
+console.log('Without new \\n line')
+
+//Unary
+console.log(-(23 + 32))
+console.log(typeof "boy")
+
+//Boolean
+console.log(2 < 6)
+
+//Logical
+console.log(0 && 6)
+console.log(false || false)
+console.log(!false)
+
+//Ternary
+console.log("batman" < "Superman" ? "The man of steel" : "or is it?")
+
+//Type Coercion
+console.log("6" - 2) //converts string to number
+console.log("4" + 3) //converts number to string
+console.log("five" * 2) // can't figure out
+console.log(false == 0) // wrong output
+console.log(false === 0) //it corrects the above line
+console.log(21 * null) //converts null to 0
+
+//there completes the kindergarden stuf
