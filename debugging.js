bd9559f47116ff080966a3ed06f3dbf31b708ff6
Vinoth kumar A <vinothkumarfrta@gmail.com>
2014-10-26 11:47:47 +0000

    complete debugging.js

---
+++ debugging.js
@@ -0,0 +1,37 @@
+//Raising an error
+function promptDirection(question) {
+  var result = prompt(question, "");
+  if (result.toLowerCase() == "left") return "L";
+  if (result.toLowerCase() == "right") return "R";
+  throw new Error("Invalid direction: " + result);
+}
+
+function look() {
+  if (promptDirection("Which way?") == "L")
+    return "a house";
+  else
+    return "two angry bears";
+}
+
+try {
+  console.log("You see", look());
+} catch (error) {
+  console.log("Something went wrong: " + error);
+}
+
+/*try block executes a block of code and if error is catched in the try,
+the subsequent catch block is executed.
+
+finally block is executed no matter what happens in the try and catch block
+*/
+
+
+function withContext(newContext, body) {
+  var oldContext = context;
+  context = newContext;
+  try {
+    return body();
+  } finally {
+    context = oldContext;
+  }
+}
